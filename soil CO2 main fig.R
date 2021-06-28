901fb16796383d8b945c12e80911ee8b0f575e50
Meghan Avolio <mavolio2@jh.edu>
2021-06-28 15:16:16 -0400

    fixing figues

--- soil CO2 main fig.R
+++ soil CO2 main fig.R
@@ -21,23 +21,22 @@ sdat2<-sdat%>%
   filter(drt!=".")%>%
   select(-heat1, -drt1, -drt2, -drt3)
 
-trtave<-sdat2%>%
+trtaves<-sdat2%>%
   group_by(year, drt)%>%
   summarize(mean=mean(soil.CO2.flux), sd=sd(soil.CO2.flux), n=length(soil.CO2.flux))%>%
   mutate(se=sd/sqrt(n))%>%
   mutate(group=ifelse(year==2012, 1, 2),
          grp=paste(group, drt, sep=""))
 
-soilresp<-ggplot(data=trtave, aes(x=year, y=mean, color=drt))+
+soilresp<-ggplot(data=trtaves, aes(x=year, y=mean, color=drt))+
   annotate("rect", xmin=2013.5, xmax=2015.5, ymin=-Inf, ymax=Inf, alpha = .2, fill="gray")+
   geom_point(size=3)+
   geom_line(aes(group=grp))+
   geom_errorbar(aes(ymin=mean-se, ymax=mean+se), width=0.075)+
   xlab("Year")+
   ylab(bquote('Soil Respiration ('*mu~ 'mol' ~CO[2]~ s^-1~m^-2*')')) +
-  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())+
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), legend.position = c(0.15,0.75))+
   scale_color_manual(name="Treatment", breaks=c("C-C", 'PD-C', "C-D", "PD-D"), labels=c("C->C", "D->C", "C->D", "D->D"), values=c("blue", "dodgerblue", "orange", "red"))+
-  annotate(x=2011.9, y=11, "text", label="C")+
   annotate(x=2014, y=11, "text", label="*", size=8)+
   annotate(x=2015, y=11, "text", label="*", size=8)
 
