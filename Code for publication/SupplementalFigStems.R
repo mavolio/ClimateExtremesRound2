8ed4331c24949bde362dea861e008a7a18d32efd
mavolio <meghan.avolio@jhu.edu>
2025-11-02 12:49:44 -0500

    making stem data and figure

--- Code for publication/SupplementalFigStems.R
+++ Code for publication/SupplementalFigStems.R
@@ -1,9 +1,5 @@
 library(tidyverse)
 library(gridExtra)
-library(lme4)
-library(lmerTest)
-library(emmeans)
-
 theme_set(theme_bw(12))
 
 setwd("C://Users//mavolio2//Dropbox//Konza Research//CEE_Part2//")
@@ -15,81 +11,53 @@ stems<-read.csv("Stem Data//All_live_springStems_11-18.csv")%>%
   left_join(trt)%>%
   filter(drt!=".") %>% 
   mutate(TotGrass=Andro+Sorg+Grass,
-       TotForb=Solidago+Forb,
-       Total=Andro+Sorg+Grass+Solidago+Forb+Wood,
-       Totnowood=Andro+Sorg+Grass+Solidago+Forb,
-       Other=Sorg+Grass+Solidago+Forb+Wood,
-       Othernowood=Sorg+Grass+Solidago+Forb)%>%
+         TotForb=Solidago+Forb,
+         Total=Andro+Sorg+Grass+Solidago+Forb+Wood,
+         Totnowood=Andro+Sorg+Grass+Solidago+Forb,
+         Other=Sorg+Grass+Solidago+Forb+Wood,
+         Othernowood=Sorg+Grass+Solidago+Forb)%>%
   mutate(drop=ifelse(Plot==210&Year==2015|Plot==102&Year==2014, 1, 0))%>%
   filter(drop!=1) %>% 
-  filter(Year>2012&Year<2017)
+  filter(Year>2012&Year<2017) %>% 
+  select(Year, Plot, Andro, Sorg, plotid,block, drt) %>% 
+  pivot_longer(Andro:Sorg, names_to = 'Species', values_to = 'Stems')
+
+#write csv for SAS
+write.csv(stems, 'C://Users//mavolio2//Dropbox//Konza Research//CEE_Part2//Analyses in SAS//stems.csv', row.names=F)
 
 
+#to plot
 stems2<-read.csv("Stem Data//All_live_springStems_11-18.csv")%>%
   left_join(trt)%>%
   filter(drt!=".") %>% 
+  mutate(drop=ifelse(Plot==210&Year==2015|Plot==102&Year==2014, 1, 0))%>%
+  filter(drop!=1)%>% 
   pivot_longer(Andro:Wood, names_to = 'sp', values_to = 'stems') %>% 
   filter(Year>2012&Year<2017) %>% 
-  group_by(Year, drt, sp) %>% 
+  group_by(drt, sp) %>% 
   summarise(m=mean(stems), sd=sd(stems), n=length(stems)) %>% 
-  mutate(se=sd/sqrt(n))
+  mutate(se=sd/sqrt(n)) %>% 
+  filter(sp %in% c('Andro', 'Sorg')) %>% 
+  mutate(sig=case_when(
+    sp=='Andro' & drt=='PD-C' ~'A', 
+    sp=='Andro' & drt=='PD-D' ~'B',
+    sp=='Andro' & drt=='C-D' ~ 'BC',
+    sp=='Andro' & drt=='C-C' ~ 'C', 
+    sp=='Sorg' & drt =='C-D' ~ 'DE',
+    sp=='Sorg' & drt =='PD-C' ~ 'E', 
+    .default = 'D'
+  ))
 
-ggplot(data=stems2, aes(x=Year, y=m, color=sp))+
-  geom_point()+
-  geom_line()+
-  geom_errorbar(aes(ymin=m-se, ymax=m+se), width=0.2)+
+stems<-
+  ggplot(data=stems2, aes(x=drt, y=m, fill=sp, label=sig))+
+  geom_bar(stat='identity', position=position_dodge(0.9))+
+  scale_fill_manual(name='Species', labels=c('A. gerardii', 'S. nutans'), values=c('black', 'gray'))+
+  scale_x_discrete(limits=c('C-C', 'PD-C', 'C-D', 'PD-D'))+
+  geom_errorbar(aes(ymin=m-se, ymax=m+se), position=position_dodge(0.9), width=0.2)+
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())+
-  xlab("Year")+
+  xlab("Drought Treatment")+
   ylab("Number of Stems")+
-  annotate("rect", xmin=2013.5, xmax=2015.5, ymin=-Inf, ymax=Inf, alpha = .2, fill="gray")+
-  facet_wrap(~drt)
+  geom_text(position=position_dodge(0.9), aes(y=m+se*2))
 
-ggplot(data=subset(stems2, sp %in% c('Andro', 'Sorg')), aes(x=Year, y=m, color=sp))+
-  geom_point()+
-  geom_line()+
-  geom_errorbar(aes(ymin=m-se, ymax=m+se), width=0.2)+
-  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())+
-  xlab("Year")+
-  ylab("Number of Stems")+
-  annotate("rect", xmin=2013.5, xmax=2015.5, ymin=-Inf, ymax=Inf, alpha = .2, fill="gray")+
-  facet_wrap(~drt)
-
-
-stems3<-read.csv("Stem Data//All_live_springStems_11-18.csv")%>%
-  left_join(trt)%>%
-  filter(drt!=".") %>% 
-  pivot_longer(Andro:Wood, names_to = 'sp', values_to = 'stems') %>% 
-  filter(Year>2012&Year<2017)
-
-
-st2013<-stems3 %>% 
-  filter(Year==2013) %>% 
-  select(plotid, stems, sp) %>% 
-  unique() %>% 
-  rename(st13=stems)
-
-stems4<-stems3 %>% 
-  left_join(st2013) %>% 
-  filter(Year!=2013) %>% 
-  mutate(pd=st13-stems) %>% 
-  group_by(Year, drt, sp) %>% 
-  summarise(m=mean(pd), sd=sd(pd), n=length(pd)) %>% 
-  mutate(se=sd/sqrt(n))
-
-mst<-lmer(stems~drt*sp*as.factor(Year)+(1|block/Plot), data=subset(stems3, sp %in% c('Andro', 'Sorg')))
-summary(mst)
-anova(mst) #use this ddf for repeated measures.
-emmeans(mst, pairwise~drt|Year|sp)
-
-ggplot(data=subset(stems2, sp %in% c('Andro', 'Sorg')), aes(x=Year, y=m, shape=sp, color=drt))+
-  geom_point(size=3)+
-  geom_line()+
-  geom_errorbar(aes(ymin=m-se, ymax=m+se), width=0.2)+
-  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())+
-  scale_color_manual(name="Treatment", breaks=c("C-C", "PD-C", "C-D", "PD-D"), labels=c("C->C", "D->C", "C->D", "D->D"), values=c("blue", "dodgerblue", "orange", "red"))+
-  xlab("Year")+
-  ylab("Number of Stems")+
-  facet_wrap(~sp)
- 
-
+ggsave('C://Users//mavolio2//Dropbox//Konza Research//CEE_Part2//Manuscript//Fig_Stems.jpeg', stems, width=8, height=8, units='in')
 
